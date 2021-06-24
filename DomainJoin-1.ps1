f836ae9bf01e080f89fc3640fca3193fd2d33627
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-06-24 11:33:26 -0500

    Update DomainJoin-1.ps1

--- DomainJoin-1.ps1
+++ DomainJoin-1.ps1
@@ -2,26 +2,33 @@ Param(
     [string]$NewComputer = $(Read-Host "Enter New computer name")
 )
 
+###################################################################################################
+#Change variable names below to reflect your environment
 $UpdatedComputer = $NewComputer + "$"
 $ComputerFile = $NewComputer + ".txt"
-$MDTServer = "PSD"
-$DomainController = "DC-Austin"
-$DirectServer = "DirectAccess" 
-$Domain = "advocacyinc.org"
-$DirectGroup = "DirectAccess Clients"
+$MDTServer = "NAMEofYOurPSDServer"
+$MDTShare = "Set PSD/MDT share folder" #mine was c:\PSDeployment
+$DomainController = "DomainControllerName"
+$DirectServer = "Direct Access Server Name" 
+$Domain = "YourActiveDirectoryDomainName"
+$DirectGroup = "DirectAccess Clients" #This is default name for the group; yours might be different
+####################################################################################################
 
+
+#Creates path inside PSD/MDT environment if not already created. 
 If ($env:COMPUTERNAME -eq $MDTServer) {
-        New-Item -ItemType directory -Path "c:\PSDeployment\OfflineJoinDomain" -ErrorAction SilentlyContinue
+        New-Item -ItemType directory -Path "c:\$MDTShare\OfflineJoinDomain" -ErrorAction SilentlyContinue
 }    else {
 Invoke-Command -ComputerName $MDTServer -ScriptBlock {
-        New-Item -ItemType directory -Path "c:\PSDeployment\OfflineJoinDomain" -ErrorAction SilentlyContinue
+        New-Item -ItemType directory -Path "c:\$MDTShare\OfflineJoinDomain" -ErrorAction SilentlyContinue
     }
 }
-
+#creates local directory and runs domain join command
         New-Item -ItemType directory -Path "c:\OfflineJoinDomain" -ErrorAction SilentlyContinue
         cmd /c Djoin.exe /provision /domain $Domain /dcname $DomainController /machine $NewComputer /policynames "DirectAccess Client Settings" /rootcacerts /savefile "c:\OfflineJoinDomain\$ComputerFile" /reuse
-        Copy-Item "c:\offlineJoinDomain\$ComputerFile" -Destination "\\psd\c$\PSDeployment\OfflineJoinDomain\$ComputerFile"  
+        Copy-Item "c:\offlineJoinDomain\$ComputerFile" -Destination "\\$MDTServer\$MDTShare\OfflineJoinDomain\$ComputerFile"  
 
+#waits 6 seconds then adds computer to necessary group for DirectAccess
 Start-Sleep 6
 If ($env:COMPUTERNAME -eq $DomainController) {
         Add-ADGroupMember -id $DirectGroup -members $UpdatedComputer
